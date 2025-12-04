252b2c6e057baedcb702e4ae666e11d7b8ec5b90
Francesco Adami <francescoadami9@gmail.com>
2025-12-05 00:51:10 +0100

    Combat Loop (attacco + cambio pokemon)

---
+++ Source/PokeLike/DataAsset/CPP_MoveDataAsset.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CPP_MoveDataAsset.generated.h"
+
+UENUM(BlueprintType) 
+enum class EMyDamageType : uint8
+{
+	Option_A UMETA(DisplayName = "Normal"),
+	Option_B UMETA(DisplayName = "Fire"),
+	Option_C UMETA(DisplayName = "Water"),
+	Option_D UMETA(DisplayName = "Grass")
+};
+
+UCLASS(BlueprintType)
+class POKELIKE_API UCPP_MoveDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+    
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MoveData")
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MoveData")
+	float Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MoveData")
+	EMyDamageType DamageType;
+};
